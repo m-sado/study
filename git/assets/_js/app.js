2a0f61d28408eeeb8082fe0350082c4458e3d82c
yonekur yuka <yuka.y@x-point-1.net>
2017-02-08 19:28:51 +0900

    アローとページャーの実装（途中）

--- git/assets/_js/app.js
+++ git/assets/_js/app.js
@@ -13,21 +13,55 @@ var device = {
 
 var page = {
 	$d : $("#document"),
-  $s : $("#slideshow"),
+  $s : $(".slideshow"),
   current:0,
-  MAX : 4,
+  MAX : 8,
   time:[],
+
+
 	/* [ resize ]
 	---------------------------------------------------------------------*/
 	init : function(){
     //console.log("test");
     page.animation(page.current);
-	},
-  animation:function(num){
 
+
+    $(".slideshow").each(function (i) {
+        $(this).css({ left: 100 * i + '%' });
+        $('.slideshow-indicator').HTML += '<a href="#">' + (i + 1) + '</a>';
+    });
+
+
+    // $("slideshow-nav a").on("click",function(){
+    //   console.log($(this).attr("id"));
+    //   if($(this).attr("id")=="next"){
+
+    //   }else{
+
+    //   }
+    // });
+
+    $(".slideshow-nav a").on('click', 'a', function (event) {
+        event.preventDefault();
+        if ($(this).hasClass('prev')) {
+            $('.slideshow').children(currentIndex - 1);
+        } else {
+            $('.slideshow').children(currentIndex + 1);
+        }
+    });
+
+    // $('.slideshow-nav a').on('click' , function(){
+    //     var idx = $(this).index();
+    //     $('.slideshow').children('gotoSlide' ,   $(this).index() );
+    //     console.log(idx);
+    //     return false;
+    // });
+
+	},
+
+  animation:function(num){
     //console.log("num",num);
     //console.log(page);
-
     TweenMax.to(page.$s.children(), 1, {
       opacity: 0,
       onComplete:function(){
@@ -43,12 +77,11 @@ var page = {
             clearTimeout(page.time);
             page.time = setTimeout(function(){
               page.animation(page.current);
-            },1000);
+            },10000);
           }
         });
       }
     });
-
   }
 
 }
@@ -57,7 +90,6 @@ var page = {
 $w.on("load",function(){
 	page.init();
 });
-
 
 
 
