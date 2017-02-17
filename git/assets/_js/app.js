c59754c227877094ca3a56e335a86428595dcd19
motoki sado <motoki.s@x-point-1.net>
2017-02-17 10:09:06 +0900

    アローマークとページャーのjsの修正

--- git/assets/_js/app.js
+++ git/assets/_js/app.js
@@ -17,7 +17,7 @@ var page =  {
   current:0,
   MAX : 6,
   time:[],
-
+  type : "auto",
 
 	/* [ resize ]
 	---------------------------------------------------------------------*/
@@ -26,46 +26,79 @@ var page =  {
     page.animation(page.current);
 
     var indicatorHTML = "";
-    page.$s.find('img').each(function (i) {
-        indicatorHTML += '<a href="#">' + (i + 1) + '</a>';
-    });
+    // page.$s.find('img').each(function (i) {
+    //     indicatorHTML += '<a href="#">' + (i + 1) + '</a>';
+    // });
+
+    for(var i=0; i < page.$s.find('img').length; i++){
+      indicatorHTML += '<a href="#">' + (i + 1) + '</a>';
+    }
+
     $('.slideshow-indicator').html(indicatorHTML);
 
     $(".slideshow-indicator").on('click', 'a', function (event) {
         event.preventDefault();
         if (!$(this).hasClass('active')) {
-          var number = $(this).index();
-              numberIndex = number+1;
+          // var number = $(this).index();
+          //     numberIndex = number+1;
           // console.log(numberIndex);
             // $('.slideshow').children(currentIndex - 1);
-            page.animation(numberIndex);
+            page.current = $(this).index();
+            page.animation("ctrl");
         }
     });
 
 
     $(".arrow").on('click', function (event) {
+      page.type="ctrl";
       // console.log('テスト');
         event.preventDefault();
         if ($(this).hasClass('prev')) {
          // console.log('テスト');
-          page.animation(page.current--);
+          page.animation("prev");
         } else {
           // console.log('テスト2');
-            page.animation(page.current++);
+            page.animation("next");
           }
 
-        if (page.current >= page.MAX) {
-          page.current=0;
-        } else if (page.current <= 0) {
-          page.current=page.MAX;
-        }
+        // if (page.current >= page.MAX) {
+        //   page.current=0;
+        // } else if (page.current <= 0) {
+        //   page.current=page.MAX;
+        // }
 
     });
 
 	},
 
-  animation:function(num){
-    console.log("num",num);
+  update : function(dir){
+    // console.log("update");
+      if(page.type == "auto"){
+        if(page.current < page.MAX){
+          page.current++;
+        }else{
+          page.current=0;
+        }
+      }else{
+
+        if(dir == "next"){
+          page.current++;
+        }else{
+          page.current--;
+        }
+
+        // if (page.current >= page.MAX) {
+        //   page.current=0;
+        // } else if (page.current <= 0) {
+        //   page.current=page.MAX;
+        // }
+
+
+      }
+  },
+
+  animation:function(dir){
+    console.log("page.current",page.current);
     //console.log(page);
     TweenMax.to(page.$s.children(), 1, {
       opacity: 0,
@@ -74,15 +107,18 @@ var page =  {
           opacity: 1,
           ease : Power1.easeOut,
           onComplete:function(){
-            if(page.current < page.MAX){
-              page.current++;
-            }else{
-              page.current=0;
+            page.update(dir);
+            // if(page.current < page.MAX){
+            //   page.current++;
+            // }else{
+            //   page.current=0;
+            // }
+            if(page.type == "auto") {
+              clearTimeout(page.time);
+              page.time = setTimeout(function(){
+                page.animation(page.current);
+              },1000);
             }
-            clearTimeout(page.time);
-            page.time = setTimeout(function(){
-              page.animation(page.current);
-            },1000);
           }
         });
       }
